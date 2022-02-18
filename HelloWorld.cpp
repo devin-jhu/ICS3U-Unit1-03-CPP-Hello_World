c1da7d6ec2c120f68532579b2e3b411d8236cc11
Devin Jhu <devin.jhu@mths.ca>
2022-02-18 21:28:42 +0000

    added hello wolrd

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2022 Devin Jhu All rights reserved
+//
+// Created by Devin Jhu
+// Created on February 2022
+// The proper format "Hello, World!"
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World" << std::endl;
+}
